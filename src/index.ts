b8dc6790b86010f26baf0eee1e6f8224e841104a
Joel Lara <joellara445@gmail.com>
2024-06-09 18:36:35 -0500

    deV:

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const createApp = require('./server/server');
 const config = require('./config/config.ts');
 const hostname = 'localhost';
 const app = createApp();
-
+const PORT = process.env.PORT || 3000;
 console.log('Conexión a la base de datos establecida...');
-app.listen(3000, hostname, () => {
+app.listen(PORT, hostname, () => {
   console.log(`API REST corriendo en http://${hostname}:3000/`);
 });
